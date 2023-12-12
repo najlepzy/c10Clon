1e7edb683b7b2558939bf9f9d7ec55c1056938d0
najlepzy <lautanaj@hotmail.com>
2023-12-12 15:39:31 -0300

    commit

---
+++ file.js
@@ -0,0 +1,10 @@
+function realizarCalculos(a, b) {
+  console.log("Suma:", a + b);
+  console.log("Resta:", a - b);
+  console.log("Multiplicación:", a * b);
+  console.log("División:", a / b);
+  console.log("Módulo:", a % b);
+}
+
+// Llamamos a la función con dos números de ejemplo
+realizarCalculos(10, 5);
---
+++ file2.js
@@ -0,0 +1,11 @@
+function operacionesMatematicas(numero1, numero2) {
+    console.log('Suma:', numero1 + numero2);
+    console.log('Resta:', numero1 - numero2);
+    console.log('Multiplicación:', numero1 * numero2);
+    console.log('División:', numero1 / numero2);
+    console.log('Módulo:', numero1 % numero2);
+  }
+  
+  // Llamamos a la función con dos números de ejemplo
+  operacionesMatematicas(15, 6);
+  
